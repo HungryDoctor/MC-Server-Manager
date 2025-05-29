ee34e10d3183968307181e926928f551e8cf6445
HungryDoctor <garmatin.vyacheslav@gmail.com>
2025-05-29 21:47:28 +0200

    WIP

---
+++ src/system/Tests/LifecycleTests/LiteDbServerProcessRepositoryTests.cs
@@ -0,0 +1,32 @@
+﻿using LiteDB;
+using Services.Lifecycle.Repository;
+using System.IO;
+
+namespace LifecycleTests
+{
+    public class LiteDbServerProcessRepositoryTests
+    {
+        private MemoryStream m_dbMemoryStream;
+        private LiteDatabase m_database;
+        private LiteDbServerProcessRepository m_liteDbServerProcessRepository;
+
+
+        [Before(HookType.Test)]
+        public void InitializeTest()
+        {
+            m_dbMemoryStream = new MemoryStream();
+            m_database = new LiteDatabase(m_dbMemoryStream);
+            m_liteDbServerProcessRepository = new LiteDbServerProcessRepository(m_database);
+        }
+
+        [After(HookType.Test)]
+        public void CleanupTest()
+        {
+            m_database.Dispose();
+            m_dbMemoryStream.Dispose();
+        }
+
+
+
+    }
+}
