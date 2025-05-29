66b6c73180408ac413aa9b159f851db7af2a35d6
HungryDoctor <garmatin.vyacheslav@gmail.com>
2025-05-29 20:46:41 +0200

    Added ServerProcessRepository

---
+++ src/system/Services/Services.Lifecycle/Repository/IServerProcessRepository.cs
@@ -0,0 +1,15 @@
+﻿using Contracts.Lifecycle;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Services.Lifecycle.Repository
+{
+    public interface IServerProcessRepository
+    {
+        Task<ServerState> LoadAsync(ServerInstanceId serverInstanceId, CancellationToken ct = default);
+        Task<IReadOnlySet<ServerState>> ListAsync(CancellationToken ct = default);
+        Task<ServerInstanceId> CreateAsync(ServerState serverState, CancellationToken ct = default);
+        Task SaveAsync(ServerState serverState, CancellationToken ct = default);
+    }
+}
---
+++ src/system/Services/Services.Lifecycle/Repository/LiteDbServerProcessRepository.cs
@@ -0,0 +1,52 @@
+﻿using Contracts.Lifecycle;
+using LiteDB;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Services.Lifecycle.Repository
+{
+    public class LiteDbServerProcessRepository : IServerProcessRepository
+    {
+        private const string c_serverProcess = "server_process";
+        private readonly ILiteDatabase m_db;
+
+
+        public LiteDbServerProcessRepository(ILiteDatabase db)
+        {
+            m_db = db;
+        }
+
+
+        public Task<ServerInstanceId> CreateAsync(ServerState serverState, CancellationToken ct = default)
+        {
+            m_db.GetCollection<ServerState>(c_serverProcess).Insert(serverState.GetBsonKey(), serverState);
+            return Task.FromResult(serverState.ServerInstanceId);
+        }
+
+        public Task<IReadOnlySet<ServerState>> ListAsync(CancellationToken ct = default)
+        {
+            IReadOnlySet<ServerState> result = m_db.GetCollection<ServerState>(c_serverProcess).FindAll().ToHashSet();
+            return Task.FromResult(result);
+        }
+
+        public Task<ServerState> LoadAsync(ServerInstanceId serverInstanceId, CancellationToken ct = default)
+        {
+            ServerState result = m_db.GetCollection<ServerState>(c_serverProcess).FindById(serverInstanceId.GetBsonKey());
+            return Task.FromResult(result);
+        }
+
+        public Task SaveAsync(ServerState serverState, CancellationToken ct = default)
+        {
+            m_db.GetCollection<ServerState>(c_serverProcess).Upsert(serverState.GetBsonKey(), serverState);
+            return Task.CompletedTask;
+        }
+    }
+
+    file static class ServerInstanceIdExtensions
+    {
+        public static BsonValue GetBsonKey(this ServerInstanceId serverInstanceId) => serverInstanceId.Value;
+        public static BsonValue GetBsonKey(this ServerState serverState) => serverState.ServerInstanceId.Value;
+    }
+}
